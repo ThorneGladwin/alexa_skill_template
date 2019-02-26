14f5c00ce5d66da7b455b0b5a25418cc16b1364c
Thorne Gladwin <thorne.gladwin@eurostar.com>
2019-02-26 09:19:17 +0000

    Added intent logic

---
+++ lambda/custom/handlers/core/exitRequestHandler.js
@@ -0,0 +1,19 @@
+module.exports = {
+    canHandle(handlerInput) {
+      const request = handlerInput.requestEnvelope.request;
+  
+      return (
+        request.type === "IntentRequest" &&
+        (request.intent.name === "AMAZON.CancelIntent" ||
+          request.intent.name === "AMAZON.StopIntent")
+      );
+    },
+    handle(handlerInput) {
+      const speechOutput = langHelper.resourceData(handlerInput).CoreIntents;
+  
+      return handlerInput.responseBuilder
+        .speak(speechOutput.ExitRequest.OutputSpeech)
+        .withShouldEndSession(true)
+        .getResponse();
+    }
+  };
